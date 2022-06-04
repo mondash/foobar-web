2ec13b14f3fb4a7b0e2dd0f7028527058589e474
Matthew Ondash <ondashm@gmail.com>
2022-06-04 00:09:06 -0500

    fix jest and add eslint

--- jest.config.ts
+++ jest.config.ts
@@ -2,6 +2,7 @@ import nextJest from "next/jest";
 import type { Config } from "@jest/types";
 
 const initialConfig: Config.InitialOptions = {
+  setupFilesAfterEnv: ["./jest.setup.ts"],
   testEnvironment: "jsdom",
 };
 const createJestConfig = nextJest({ dir: "." });
--- test/_foo.test.tsx
+++ test/_foo.test.tsx
@@ -1,11 +1,24 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
-const Foo = (): JSX.Element => <div>bar</div>;
+const Foo = (): JSX.Element => {
+  const [count, setCount] = React.useState(0);
+  const incrementCount = () => setCount(current => current + 1);
 
-test("qwerty", () => {
+  return (
+    <div id="foo" onClick={incrementCount}>
+      Count: {count}
+    </div>
+  );
+};
+
+test("qwerty", async () => {
   render(<Foo />);
-  expect(undefined).toBeUndefined();
+  const foo = screen.getByText(/count/i);
+  expect(foo).toHaveTextContent(/0/);
+  await userEvent.click(foo);
+  expect(foo).toHaveTextContent(/1/);
 });
 
 describe("bar", () => {
